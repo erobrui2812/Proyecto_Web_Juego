fe05a9307ea7c087f0ea80e1920e40c589236a59
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-01-09 16:00:06 +0100

    Manejo de registro y login desde el back hecho y funcional

--- backend/hundir-la-flota_Server/hundir-la-flota/Program.cs
+++ backend/hundir-la-flota_Server/hundir-la-flota/Program.cs
@@ -12,17 +12,26 @@ namespace hundir_la_flota
 
             // Add services to the container.
 
+            string dbPath = Path.Combine(AppContext.BaseDirectory, "hundir_la_flota.db");
+            string connectionString = $"Data Source={dbPath};";
+
             builder.Services.AddDbContext<MyDbContext>(options =>
-            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+               options.UseSqlite(connectionString));
 
 
 
-            builder.Services.AddSingleton<AuthService>(new AuthService("SecretJWTKey"));
+            builder.Services.AddSingleton<AuthService>(new AuthService("245324765298376324895324987645328976"));
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+                dbContext.Database.EnsureCreated(); // Crea la base de datos y tablas si no existen
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
