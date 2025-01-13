7eafa31c20d700903c2449497b60f0172e736f8a
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-01-13 19:54:43 +0100

    hub para el manejo del estado de los usuarios

---
+++ backend/hundir-la-flota_Server/hundir-la-flota/Hubs/StatusHub.cs
@@ -0,0 +1,34 @@
+﻿
+namespace hundir_la_flota.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+    using System.Threading.Tasks;
+
+    public class StatusHub : Hub
+    {
+        
+        public async Task UpdateUserStatus(int userId, string status)
+        {
+            
+            
+            await Clients.All.SendAsync("ReceiveUserStatus", userId, status);
+        }
+
+        // Cuando un cliente se conecta
+        public override async Task OnConnectedAsync()
+        {
+            var userId = Context.UserIdentifier;                                      
+            await UpdateUserStatus(int.Parse(userId), "Conectado");
+            await base.OnConnectedAsync();
+        }
+
+        // Cuando un cliente se desconecta
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var userId = Context.UserIdentifier;  
+            await UpdateUserStatus(int.Parse(userId), "Desconectado");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+
+}
