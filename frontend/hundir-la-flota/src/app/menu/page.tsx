a3951baca39459b7a1acd304de3180e360be570d
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-01-17 18:12:32 +0100

    estilizado menu un poco

--- frontend/hundir-la-flota/src/app/menu/page.tsx
+++ frontend/hundir-la-flota/src/app/menu/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { Trash2, Search, LogOut, User } from "lucide-react";
-import { useGlobalContext } from "../contexts/GlobalContext";
+import { LogOut, Search, Trash2 } from "lucide-react";
+import { useEffect, useState } from "react";
 import Modal from "../components/Modal";
+import { useGlobalContext } from "../contexts/GlobalContext";
 
 type PendingRequest = {
   id: string;
@@ -32,8 +32,13 @@ const MenuPage = () => {
     friend.nickname
       .toLowerCase()
       .normalize("NFD")
-      .replace(/[̀-ͯ]/g, "")
-      .includes(searchTerm.toLowerCase().normalize("NFD").replace(/[̀-ͯ]/g, ""))
+      .replace(/\p{Diacritic}/gu, "")
+      .includes(
+        searchTerm
+          .toLowerCase()
+          .normalize("NFD")
+          .replace(/\p{Diacritic}/gu, "")
+      )
   );
 
   const handleRemoveFriend = (friendId: string) => {
@@ -59,7 +64,10 @@ const MenuPage = () => {
       const data = await response.json();
       setPendingRequests(data);
     } catch (error) {
-      console.error("Error al obtener solicitudes de amistad pendientes:", error);
+      console.error(
+        "Error al obtener solicitudes de amistad pendientes:",
+        error
+      );
     }
   };
 
@@ -82,12 +90,14 @@ const MenuPage = () => {
           <img
             src={userDetail?.avatarUrl}
             alt="User Avatar"
-            className="w-10 h-10 rounded-full"
+            className="w-12 h-12 rounded-full border-2 border-gold"
           />
           <div>
-            <h1 className="text-xl font-bold">{userDetail?.nickname}</h1>
+            <h1 className="text-xl font-bold text-gold">
+              {userDetail?.nickname}
+            </h1>
             <button
-              className="text-blue-500 hover:underline"
+              className="text-blue-400 hover:underline"
               onClick={() => alert("Ir al perfil")}
             >
               Ver Perfil
@@ -109,21 +119,21 @@ const MenuPage = () => {
           placeholder="Buscar amigos o usuarios..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="border p-2 rounded-md w-full"
+          className="border p-2 rounded-md w-full focus:ring-2 focus:ring-primary"
         />
         <button
           onClick={() => {
             setIsSearchModalOpen(true);
             handleSearch();
           }}
-          className="p-2 bg-blue-500 text-white rounded-md flex items-center space-x-2"
+          className="p-2 bg-blue-500 text-white rounded-md flex items-center space-x-2 hover:bg-blue-600"
         >
           <Search size={20} />
           <span>Buscar Usuarios</span>
         </button>
         <button
           onClick={() => setIsRequestsModalOpen(true)}
-          className="p-2 bg-green-500 text-white rounded-md"
+          className="p-2 bg-green-500 text-white rounded-md hover:bg-green-600"
         >
           Solicitudes de Amistad
         </button>
@@ -133,16 +143,15 @@ const MenuPage = () => {
         {filteredFriends.map((friend) => (
           <div
             key={friend.id}
-            className="flex items-center space-x-4 p-4 border-b"
+            className="flex items-center space-x-4 p-4 bg-gray-800 rounded-md shadow-md"
           >
             <img
               src={friend.urlAvatar}
               alt={`${friend.nickname}'s Avatar`}
-              className="w-8 h-8 rounded-full"
+              className="w-10 h-10 rounded-full border-2 border-secondary"
             />
             <div className="flex flex-col">
-              <span className="font-semibold">{friend.nickname}</span>
-              <span className="text-sm text-gray-500">{friend.email}</span>
+              <span className="font-semibold text-gold">{friend.nickname}</span>
               <span
                 className={`text-sm ${
                   friend.status === "Conectado"
@@ -165,7 +174,7 @@ const MenuPage = () => {
 
       <button
         onClick={() => alert("Ir a Emparejamiento")}
-        className="p-4 bg-blue-500 text-white rounded-md w-full text-center"
+        className="p-4 bg-primary text-white rounded-md w-full text-center hover:bg-wine"
       >
         Jugar
       </button>
@@ -179,7 +188,7 @@ const MenuPage = () => {
           {searchResults.map((user) => (
             <div
               key={user.id}
-              className="flex items-center justify-between p-4 border rounded-md"
+              className="flex items-center justify-between p-4 bg-gray-700 rounded-md"
             >
               <div className="flex items-center space-x-4">
                 <img
@@ -187,11 +196,11 @@ const MenuPage = () => {
                   alt={`${user.nickname}'s Avatar`}
                   className="w-8 h-8 rounded-full"
                 />
-                <p className="font-semibold">{user.nickname}</p>
+                <p className="font-semibold text-gold">{user.nickname}</p>
               </div>
               <button
                 onClick={() => sendFriendRequest(user.id)}
-                className="p-2 bg-green-500 text-white rounded-md"
+                className="p-2 bg-green-500 text-white rounded-md hover:bg-green-600"
               >
                 Enviar Solicitud
               </button>
@@ -209,13 +218,14 @@ const MenuPage = () => {
           {pendingRequests.map((request) => (
             <div
               key={request.id}
-              className="flex items-center justify-between p-4 border rounded-md"
+              className="flex items-center justify-between p-4 bg-gray-700 rounded-md"
             >
               <div>
-                <p className="font-semibold">{request.fromUserNickname}</p>
-                <p className="text-sm text-gray-500">
-                  Enviado el:{" "}
-                  {new Date(request.createdAt).toLocaleDateString()}
+                <p className="font-semibold text-gold">
+                  {request.fromUserNickname}
+                </p>
+                <p className="text-sm text-silver">
+                  Enviado el: {new Date(request.createdAt).toLocaleDateString()}
                 </p>
               </div>
               <div className="flex space-x-2">
@@ -226,7 +236,7 @@ const MenuPage = () => {
                       prev.filter((r) => r.id !== request.id)
                     );
                   }}
-                  className="p-2 bg-green-500 text-white rounded-md"
+                  className="p-2 bg-green-500 text-white rounded-md hover:bg-green-600"
                 >
                   Aceptar
                 </button>
@@ -240,7 +250,7 @@ const MenuPage = () => {
                       prev.filter((r) => r.id !== request.id)
                     );
                   }}
-                  className="p-2 bg-red-500 text-white rounded-md"
+                  className="p-2 bg-red-500 text-white rounded-md hover:bg-red-600"
                 >
                   Rechazar
                 </button>
