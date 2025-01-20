b532f6c64e1e77e48fcbe394613240e1f1092bba
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-01-20 19:13:48 +0100

    implementada redireccion en login y registro

--- frontend/hundir-la-flota/src/app/registro/page.tsx
+++ frontend/hundir-la-flota/src/app/registro/page.tsx
@@ -1,7 +1,19 @@
-import Link from "next/link";
+"use client";
+
 import RegisterForm from "@/components/RegisterForm";
+import { useAuth } from "@/contexts/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const RegistroPage = () => {
+  const router = useRouter();
+  const { userDetail } = useAuth();
+  useEffect(() => {
+    if (userDetail) {
+      router.push("/menu");
+    }
+  }, [userDetail, router]);
+
   return (
     <div className="flex justify-center items-center h-screen bg-background">
       <div className="bg-white shadow-md rounded-lg p-6 w-full sm:w-96">
@@ -12,12 +24,12 @@ const RegistroPage = () => {
         <div className="mt-4 text-center">
           <p className="text-sm text-gray-600">
             ¿Ya tienes cuenta?{" "}
-            <Link
+            <a
               href="/login"
               className="text-primary hover:text-wine font-semibold"
             >
               Inicia sesión
-            </Link>
+            </a>
           </p>
         </div>
       </div>
